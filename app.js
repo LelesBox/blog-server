6c9d7a6ab77021dba06c84fd56ec909c64b3a097
LelesBox <li_xiaoliang@foxmail.com>
2016-03-14 14:39:34 +0800

    日志时间固定为北京时间

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 "use strict";
 
 let rhyme = require("./lib/rhyme-core")
+var log = require('./lib/log')
+var utils = require('./lib/utils')
+// logger
+rhyme.use(function *(next) {
+    var start = new Date
+    yield next
+    var ms = new Date - start
+    var req = this.req
+    var ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    log.info(this.method, this.url, this.response.status, "ip " + ip, ms + "ms", utils.beijingTime())
+})
 
-rhyme
-    .lift(function (r) {
-        console.log("bootstrap on port " + r.config.port, new Date())
-    });
+rhyme.lift(function (r) {
+    console.log("bootstrap on port " + r.config.port, new Date())
+});
